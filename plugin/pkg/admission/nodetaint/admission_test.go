9736f831fc55c94b15244d6f6c19ab035a5b0631
ravisantoshgudimetla <ravisantoshgudimetla@gmail.com>
2017-03-17 12:04:19 -0400

    Added a trivial test case

--- plugin/pkg/admission/nodetaint/admission_test.go
+++ plugin/pkg/admission/nodetaint/admission_test.go
@@ -26,3 +26,57 @@ import (
 )
 
 // ensures the hard PodAntiAffinity is denied if it defines TopologyKey other than kubernetes.io/hostname.
+func TestOtherResources(t *testing.T) {
+	namespace := "testnamespace"
+	name := "testname"
+	node := &api.Node{
+		ObjectMeta: metav1.ObjectMeta{Name: name},
+	}
+	tests := []struct {
+		name        string
+		kind        string
+		resource    string
+		subresource string
+		object      runtime.Object
+		expectError bool
+	}{
+		{
+			name:     "non-pod resource",
+			kind:     "Foo",
+			resource: "foos",
+			object:   node,
+		},
+		{
+			name:        "pod subresource",
+			kind:        "Node",
+			resource:    "nodes",
+			subresource: "eviction",
+			object:      node,
+		},
+		{
+			name:        "non-pod object",
+			kind:        "Node",
+			resource:    "pods",
+			object:      node,
+			expectError: false,
+		},
+	}
+
+	for _, tc := range tests {
+		handler := &plugin{}
+
+		err := handler.Admit(admission.NewAttributesRecord(tc.object, nil, api.Kind(tc.kind).WithVersion("version"), namespace, name, api.Resource(tc.resource).WithVersion("version"), tc.subresource, admission.Create, nil))
+
+		if tc.expectError {
+			if err == nil {
+				t.Errorf("%s: unexpected nil error", tc.name)
+			}
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("%s: unexpected error: %v", tc.name, err)
+			continue
+		}
+	}
+}
